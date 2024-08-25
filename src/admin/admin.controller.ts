35221603f42e3adf16fa678b82f77119781e8f59
KHab1bull0 <habibullotoyrilmasov@gmail.com>
2024-08-25 15:18:05 +0500

    admin signup and login api added

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,14 +1,21 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { SignupAdminDto } from './dto/admin.signup';
+import { SigninAdminDto } from './dto/admin.signin';
 
 @Controller('admin')
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 
-  @Post('sidnup')
+  @Post('signup')
   create(@Body() signupAdminDto: SignupAdminDto) {
     return this.adminService.create(signupAdminDto);
+  }
+
+
+  @Post('login')
+  login(@Body() signinAdminDto: SigninAdminDto){
+    return this.adminService.login(signinAdminDto)
   }
 
 }
