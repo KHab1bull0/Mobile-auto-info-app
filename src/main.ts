1849a036fd7e4cc6ff4492dba02246b73d49f93a
KHab1bull0 <habibullotoyrilmasov@gmail.com>
2024-08-26 10:21:22 +0500

    prava and cars api fixed and added

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,9 @@ async function bootstrap() {
 
   app.setGlobalPrefix("api")
   app.enableCors({
-    origin: 'http:/3.120.192.20:6000', 
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
+    origin: '*',
   });
-  
+
   const port = process.env.PORT || 4000
   await app.listen(port, () => {
     console.log("Server is working on port ", port)
