c714cf41992c1591a7c148516c486826c065912b
KHab1bull0 <habibullotoyrilmasov@gmail.com>
2024-09-04 09:24:37 +0500

    bug fixed jarima module error fixed

--- src/jarima/jarima.module.ts
+++ src/jarima/jarima.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { JarimaService } from './jarima.service';
 import { JarimaController } from './jarima.controller';
+import { PrismaService } from 'src/helper/prisma.service';
+import { HashService } from 'src/helper/hash.service';
 
 @Module({
   controllers: [JarimaController],
-  providers: [JarimaService],
+  providers: [JarimaService, PrismaService, HashService],
 })
 export class JarimaModule {}
